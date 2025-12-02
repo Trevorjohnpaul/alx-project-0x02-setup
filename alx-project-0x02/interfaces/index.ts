44266a5f07759eb8c58895a9d16aa5ebb395d491
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-12-02 09:34:26 +0300

    interfaces fix

---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,38 @@
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone?: string;
+  website?: string;
+}
+
+export interface IPost {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+export interface IButtonProps {
+  label: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  variant?: "primary" | "secondary";
+  className?: string;
+}
+
+export interface ICardProps {
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export interface IPostCardProps {
+  post: IPost;
+  onClick: () => void;
+}
+
+export interface IUserCardProps {
+  user: IUser;
+}
