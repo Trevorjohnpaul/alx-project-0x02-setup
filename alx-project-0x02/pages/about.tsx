df5273e1c923a1e78414c87fa1f9eaa55f3ea2cd
Trevor Johnpaul <trevorjohnpaul4@gmail.com>
2025-12-02 11:16:04 +0300

    correction

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,7 @@
-// pages/about.tsx
 import React from "react";
 import Button from "@/components/common/Button";
 
-const AboutPage = () => {
+const AboutPage: React.FC = () => {
   return (
     <div className="p-8 space-y-4">
       <h1 className="text-2xl font-bold">About Page</h1>
